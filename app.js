79478ce45da00900a37a3fd9191f15dc0daa31dc
DakshalP <dakshal29@gmail.com>
2020-10-01 11:21:32 -0400

    redo first commit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const socket = require("socket.io")
+//const socket = require("socket.io")
 
 
 
@@ -10,11 +10,4 @@ app.use(express.static("public"));
 
 const server = app.listen(4000, () => {
     console.log("Server listening to requests on port 4000");
-})
-
-//socket setup
-const io = socket(server);
-
-io.on('connection', (socket) => {
-    console.log("Made a socket connection!", socket.id);
 })
